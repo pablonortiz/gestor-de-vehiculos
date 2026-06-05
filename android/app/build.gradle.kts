5e167b13aa74fc96ca15af311d7861acfcfa18ff
Pablo Nicolás Ortiz <pablonortiz05@hotmail.com>
2026-06-05 15:41:54 -0300

    Build: habilitar core library desugaring (requerido por flutter_local_notifications)
    flutter_local_notifications (#Ft1) requiere coreLibraryDesugaring para el build
    release de Android. Se habilita en compileOptions + se agrega desugar_jdk_libs.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,6 +11,8 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        // Requerido por flutter_local_notifications (recordatorios #Ft1).
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -47,4 +49,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
